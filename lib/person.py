aec413068673b67a65cb7a8900a5e540e2a4ce50
Patty Karigi Kariuki <pattykarigi96@gmail.com>
2025-02-20 14:14:45 +0300

    Initial commit

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,25 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, job = "Unemployed"):
+        self.job = job
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self._job = "Unemployed"
+        else:
+            self._job = job
+
+    job = property(get_job, set_job)
+
+person = Person()
+person.job = "Admin"
+print(person.job)
+person.job = "Pilot"
+print(person.job)
+person.job = ""
+print(person.job)
